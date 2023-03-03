9c023fad3fc0269aac3f24d3402e4fce2ebe3139
twój login do GitHuba <twj email>
2023-03-03 09:58:14 +0100

    🔧 added configuration files

---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  mode: "none",
+  entry: `./js/app.js`,
+  devtool: "inline-source-map",
+  output: {
+    filename: "out.js",
+    path: path.resolve(__dirname, "build"),
+    clean: true,
+  },
+  devServer: {
+    open: true,
+    hot: true,
+    static: [
+      {
+        directory: path.join(__dirname),
+        publicPath: "/",
+        serveIndex: true,
+      },
+    ],
+    devMiddleware: {
+      writeToDisk: true,
+    },
+    compress: true,
+    port: 3001,
+    historyApiFallback: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      process: "process/browser",
+    }),
+  ],
+};
